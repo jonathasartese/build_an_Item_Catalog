2b5430f9f8b10c2592934bb6eb2e0fd3bb73bc6b
Jonathas Thiberio Artese da Silva <jonathas.artese@gmail.com>
2017-10-26 21:50:03 -0400

    Add files via upload

--- itemscatalog.py
+++ itemscatalog.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import User, Base, Category, CatalogItem
 
-engine = create_engine('sqlite:///catalogitemsandusers.db')
+engine = create_engine('sqlite:///catalogitemswithusers.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
